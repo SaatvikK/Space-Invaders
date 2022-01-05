77241244c183cb0db22288a2c00d3a35558be8d1
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2022-01-05 18:09:12 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -18,7 +18,22 @@ class Main():
   def main(self):
     newmenu = mainMenu();
     newmenu.menuStart();
-    newgame = game(int(newmenu.Lives), newmenu.cooldowns);
+    if(newmenu.type == "create"): 
+      newgame = game(int(newmenu.Lives), newmenu.cooldowns, newmenu.difficulty);
+      newgame.GameID = newgame.generateGameID();
+      newgame.createSprites();
+      newgame.makeAliens();
+      newgame.saveGame();
+      newgame.gameLoop();
+
+    elif(newmenu.type == "load"):
+      newgame = game(0, {"alien": 0, "player": 0, "AlienBulletsMax": 0}, "easy");
+      newgame.createSprites();
+      newgame.load(newmenu.GameID);
+      newgame.makeAliens();
+      newgame.saveGame();
+      newgame.gameLoop();
+
 
 
 if(__name__ == "__main__"): 
